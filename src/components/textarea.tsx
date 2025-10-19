572777506194892f7d2e3c0ec7520451ff50c74b
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 19:59:02 +0800

    add custom text area

---
+++ src/components/textarea.tsx
@@ -0,0 +1,30 @@
+export default function Textarea() {
+  return (
+    <div className="relative">
+      <textarea
+        id="hs-floating-textarea-underline"
+        className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 sm:text-sm placeholder:text-transparent focus:border-blue-500 focus:border-t-transparent focus:border-x-transparent focus:border-b-blue-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600 dark:focus:border-b-neutral-600
+    focus:pt-6
+    focus:pb-2
+    not-placeholder-shown:pt-6
+    not-placeholder-shown:pb-2
+    autofill:pt-6
+    autofill:pb-2"
+        placeholder="This is a textarea placeholder"
+        data-hs-textarea-auto-height=""
+      ></textarea>
+      <label
+        htmlFor="hs-floating-textarea-underline"
+        className="absolute top-0 start-0 py-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+      peer-focus:text-xs
+      peer-focus:-translate-y-1.5
+      peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+      peer-not-placeholder-shown:text-xs
+      peer-not-placeholder-shown:-translate-y-1.5
+      peer-not-placeholder-shown:text-gray-500 dark:peer-not-placeholder-shown:text-neutral-500 dark:text-neutral-500"
+      >
+        Comment
+      </label>
+    </div>
+  );
+}
