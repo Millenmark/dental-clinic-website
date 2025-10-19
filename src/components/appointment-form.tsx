58185935b233e2957938f4ad2909b0504d392d82
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 19:34:53 +0800

    fix input

--- src/components/appointment-form.tsx
+++ src/components/appointment-form.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useTreatments } from "../hooks/use-treatment";
+import Input from "./input";
 
 interface FormData {
-  name: string;
+  firstName: string;
   email: string;
   phone: string;
   date: string;
@@ -13,7 +14,7 @@ interface FormData {
 
 export default function AppointmentForm() {
   const [formData, setFormData] = useState<FormData>({
-    name: "",
+    firstName: "",
     email: "",
     phone: "",
     date: "",
@@ -51,23 +52,13 @@ export default function AppointmentForm() {
       </h2>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div className="grid grid-cols-2 gap-2">
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              First Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
+          <Input
+            type="text"
+            label="First Name"
+            id="firstName"
+            name="firstName"
+            onChange={handleChange}
+          />
 
           <div>
             <label
@@ -171,7 +162,6 @@ export default function AppointmentForm() {
               name="time"
               value={formData.time}
               onChange={handleChange}
-              required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
           </div>
