de62a4a4ffa461b1d41a4fa9f648ca0831f472f2
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 21:41:23 +0800

    fix style of input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -8,7 +8,7 @@ export default function Input({ id, label, ...props }: InputProps) {
     <div className="relative">
       <input
         id={id}
-        className="peer py-4 px-0 block w-full bg-transparent outline-0 border-t-transparent border-b-2 border-x-transparent border-b-gray-200 sm:text-sm placeholder:text-transparent focus:border-t-transparent focus:border-x-transparent focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400dark:focus:border-b-neutral-600
+        className="peer py-4 px-0 block w-full bg-transparent outline-0 border-t-transparent border-b-2 border-x-transparent border-b-brand-dark sm:text-sm placeholder:text-transparent focus:border-t-transparent focus:border-x-transparent focus:ring-0 disabled:opacity-50 disabled:pointer-events-none  dark:text-neutral-400dark:focus:border-b-neutral-600
     focus:pt-6
     focus:pb-2
     not-placeholder-shown:pt-6
@@ -19,15 +19,15 @@ export default function Input({ id, label, ...props }: InputProps) {
       />
       <label
         htmlFor={id}
-        className="absolute top-2.5 start-0 py-4 px-0 h-full sm:text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0]  peer-disabled:opacity-50 peer-disabled:pointer-events-none
+        className="absolute font-normal text-gray-300 top-2.5 start-0 py-4 px-0 h-full sm:text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0]  peer-disabled:opacity-50 peer-disabled:pointer-events-none
       peer-focus:scale-90
       peer-focus:translate-x-0
       peer-focus:-translate-y-4.5
-      peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+      peer-focus:text-white 
       peer-not-placeholder-shown:scale-90
       peer-not-placeholder-shown:translate-x-0
       peer-not-placeholder-shown:-translate-y-4.5
-      peer-not-placeholder-shown:text-gray-500 dark:peer-not-placeholder-shown:text-neutral-500 dark:text-neutral-500"
+      peer-not-placeholder-shown:text-gray-500 "
       >
         {label}
       </label>
