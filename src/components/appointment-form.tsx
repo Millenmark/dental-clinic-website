78dbcf03c405dfe2a8a335e0a9830b6fe35dbabb
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 22:01:36 +0800

    fix position of button

--- src/components/appointment-form.tsx
+++ src/components/appointment-form.tsx
@@ -140,12 +140,14 @@ export default function AppointmentForm() {
           <Textarea id="note" label="Note" />
         </div>
 
-        <button
-          type="submit"
-          className="w-auto bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        >
-          Book
-        </button>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          >
+            Book
+          </button>
+        </div>
       </form>
     </div>
   );
