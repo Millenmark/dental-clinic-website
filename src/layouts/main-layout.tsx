9ea6774300ba35dc0737b3c3dcb3ba5012042312
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 17:55:08 +0800

    add poppins font

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-gray-50 font-poppins">
       <Navbar />
 
       {/* Main content area */}
