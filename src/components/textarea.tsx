a1e8e94e23558c45f4d11529796f809f9affa56e
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-19 21:56:41 +0800

    fix style of textarea

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -9,7 +9,7 @@ export default function Textarea({ id, label, ...props }: TextareaProps) {
     <div className="relative col-span-2">
       <textarea
         id="hs-floating-textarea-underline"
-        className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 sm:text-sm placeholder:text-transparent outline-0   disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600 dark:focus:border-b-neutral-600
+        className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-brand-dark sm:text-sm placeholder:text-transparent outline-0   disabled:opacity-50 disabled:pointer-events-none 
     focus:pt-6
     focus:pb-2
     not-placeholder-shown:pt-6
@@ -22,13 +22,13 @@ export default function Textarea({ id, label, ...props }: TextareaProps) {
       ></textarea>
       <label
         htmlFor="hs-floating-textarea-underline"
-        className="absolute top-0 start-0 py-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
+        className="absolute text-gray-300 top-0 start-0 py-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none
       peer-focus:text-xs
       peer-focus:-translate-y-3.5
-      peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+      peer-focus:text-white 
       peer-not-placeholder-shown:text-xs
       peer-not-placeholder-shown:-translate-y-3.5
-      peer-not-placeholder-shown:text-gray-500 dark:peer-not-placeholder-shown:text-neutral-500 "
+      peer-not-placeholder-shown:text-white  "
       >
         {label}
       </label>
