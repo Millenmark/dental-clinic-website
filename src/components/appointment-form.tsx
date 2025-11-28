b8897e46c7f8315d63dfc8915ffb27c7c0483190
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-11-29 03:15:21 +0800

    remove console.log

--- src/components/appointment-form.tsx
+++ src/components/appointment-form.tsx
@@ -95,8 +95,6 @@ export default function AppointmentForm() {
 
   const { data: treatments } = useTreatments();
 
-  console.log(treatments)
-
   const today = new Date();
   const minDate = today.toISOString().split("T")[0];
 
